1e5715d628edd81be5afe8ec1609850e3fe25ee0
white-gyu <white_gyu@naver.com>
2023-09-02 14:16:00 +0900

    chore: initialize gateway application

---
+++ src/main/java/com/vacanspot/searchserver/VacanSpotSearchServerApplication.java
@@ -0,0 +1,13 @@
+package com.vacanspot.searchserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacanSpotSearchServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacanSpotSearchServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vacanspot/searchserver/VacanSpotSearchServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vacanspot.searchserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacanSpotSearchServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
